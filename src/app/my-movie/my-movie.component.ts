0a6a1ef639988d16edafdfe9be4e79d7056513ab
olejandr <sora.oleksandr@gmail.com>
2021-07-01 13:18:50 +0200

    'version1'

--- src/app/my-movie/my-movie.component.ts
+++ src/app/my-movie/my-movie.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from "../shared/movies.service";
-import { find } from "rxjs/operators";
+
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
@@ -13,13 +13,7 @@ export class MyMovieComponent implements OnInit {
   movie!: any;
   movieId!: any;
 
-  constructor(public moviesService: MoviesService, private activatedRoute: ActivatedRoute, private router: Router,) {
-    // moviesService.getMovie$(1).subscribe(data => this.movie = data);
-
-
-    // this.moviesService.getMovie$(this.movieId).subscribe(data => this.movie = data);
-
-   }
+  constructor(public moviesService: MoviesService, private activatedRoute: ActivatedRoute, private router: Router,) { }
 
 
   ngOnInit(): void {
